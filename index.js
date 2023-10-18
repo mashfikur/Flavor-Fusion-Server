da474e8e3756cd90468b3fe2f7911f085ac555f2
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-10-19 01:56:38 +0600

    post method and database post functionality created

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-
-app.get("/",(req,res) => {
-    res.send("Welcome To The Flavor Fusion Server")
-})
-
+app.get("/", (req, res) => {
+  res.send("Welcome To The Flavor Fusion Server");
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rnoho8k.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -24,7 +22,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -32,15 +30,22 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    
+    const database = client.db("flavorDB");
 
+    const productCollection = database.collection("productCollection");
 
+    app.post("/products", async (req, res) => {
+      const productInfo = req.body;
+      const result = await productCollection.insertOne(productInfo);
 
-
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -48,7 +53,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.listen(port , () => {
-    console.log(`Server is running on port ${port}`)
-} )
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
